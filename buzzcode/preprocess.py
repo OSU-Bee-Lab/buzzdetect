7f9207c01d3486caf312f9e76aa6b88bac7be277
LukeHearon <luke.e.hearon@gmail.com>
2023-10-12 17:00:32 -0400

    change preprocess.py to invoke ffmpeg for chunking pydub has no way to work without reading the whole file into memory

--- buzzcode/preprocess.py
+++ buzzcode/preprocess.py
@@ -1,13 +1,8 @@
 import os
 import re
-
-from pydub import AudioSegment
-from pydub.utils import make_chunks
-
-raw = "./localData/Karlan Forrester - Soybean Attractiveness/raw audio/fake.mp3"
-filepath_in = "./localData/.wavtest/o.wav"
-directory_raw = "./localData/Karlan Forrester - Soybean Attractiveness/raw audio"
-
+import math
+import subprocess
+import librosa
 
 def chunk_directory(directory_raw):
     rawFiles = []
@@ -21,23 +16,27 @@ def chunk_directory(directory_raw):
         chunk_out = re.sub(pattern = "raw audio", repl = "chunked audio", string = raw)
         chunk_raw(raw, chunk_out)
 
-# note: 1hr wav in this format ~1.7 GB; seems like a doable default for memory
+# note: 1hr wav in this format ~172 MB; seems like I should increase that size to ~1 GB
 # but maybe I need to balance memory usage per-thread? Divide total memory by total threads?
 def chunk_raw(raw_path, path_out_base, chunkLength_hr = 1):
-    chunk_length = int(1000 * 60 * 60 * chunkLength_hr) # in milliseconds
+    chunk_length = int(60 * 60 * chunkLength_hr) # in seconds
+    audio_length = librosa.get_duration(path = raw_path)
+    chunks = list(range(1, math.ceil(audio_length/chunk_length)))
+    #raw_path = '\'' + raw_path + '\''
 
-    f = open(raw_path, 'rb')
-    audio_bytes = f.read()
-    raw_data = AudioSegment(audio_bytes, sample_width=2, frame_rate=16000, channels=1) # Manually specify file characteristics
-
-    chunks = make_chunks(audio_segment= raw_data, chunk_length = chunk_length)
-
-    for i, chunk in enumerate(chunks):
-        chunk_start_s = (i * chunk_length)/1000
-        path_out = path_out_base + "_" + chunk_start_s.__str__() + ".wav"
+    for i in chunks:
+        chunk_start = (i-1) * chunk_length
+        chunk_end = chunk_start + chunk_length
+        path_out = path_out_base + "_" + chunk_start.__str__() + ".wav"
         print("exporting", path_out)
-        chunk.export(
-            out_f = path_out,
-            format="wav",
-            codec = "pcm_s16le"
-        )
+        subprocess.call([
+            "ffmpeg",
+            "-i", raw_path,  # Input file
+            "-n", # don't overwrite, just error out
+            "-ar", "16000",  # Audio sample rate
+            "-ac", "1",  # Audio channels
+            "-ss", str(chunk_start),  # Start time
+            "-to", str(chunk_end),  # End time
+            "-c:a", "pcm_s16le",  # Audio codec
+            path_out  # Output path
+        ])
