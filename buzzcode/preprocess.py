af44446b63ab38cb70d2e59aa2a64e237b57cb29
LukeHearon <luke.e.hearon@gmail.com>
2023-10-13 13:31:46 -0400

    update preprocess.py to split chunking functions into smaller general functions

--- buzzcode/preprocess.py
+++ buzzcode/preprocess.py
@@ -1,6 +1,5 @@
 import os
 import re
-import math
 import subprocess
 import librosa
 
@@ -16,27 +15,59 @@ def chunk_directory(directory_raw):
         chunk_out = re.sub(pattern = "raw audio", repl = "chunked audio", string = raw)
         chunk_raw(raw, chunk_out)
 
-# note: 1hr wav in this format ~172 MB; seems like I should increase that size to ~1 GB
-# but maybe I need to balance memory usage per-thread? Divide total memory by total threads?
-def chunk_raw(raw_path, path_out_base, chunkLength_hr = 1):
-    chunk_length = int(60 * 60 * chunkLength_hr) # in seconds
-    audio_length = librosa.get_duration(path = raw_path)
-    chunks = list(range(1, math.ceil(audio_length/chunk_length)))
-    #raw_path = '\'' + raw_path + '\''
+def make_chunklist(audio_length, chunk_length):
+    chunklist = []
 
-    for i in chunks:
-        chunk_start = (i-1) * chunk_length
-        chunk_end = chunk_start + chunk_length
-        path_out = path_out_base + "_" + chunk_start.__str__() + ".wav"
-        print("exporting", path_out)
-        subprocess.call([
-            "ffmpeg",
-            "-i", raw_path,  # Input file
-            "-n", # don't overwrite, just error out
-            "-ar", "16000",  # Audio sample rate
-            "-ac", "1",  # Audio channels
-            "-ss", str(chunk_start),  # Start time
-            "-to", str(chunk_end),  # End time
-            "-c:a", "pcm_s16le",  # Audio codec
-            path_out  # Output path
-        ])
+    if chunk_length > audio_length:
+        chunk_length = audio_length
+
+    start_time = 0
+    end_time = start_time + chunk_length
+    while end_time < audio_length:
+        end_time = min(start_time + chunk_length, audio_length) # end at the end of the chunk or end of the file, whichever comes first
+        time_remaining = audio_length - end_time
+
+        if time_remaining < 30: # if the remaining time is <30 seconds, combine it into the current chunk (avoids miniscule chunks)
+            end_time = audio_length
+
+        chunklist.append((start_time, end_time))
+
+        start_time = end_time
+
+    return chunklist
+
+def take_chunk(chunktuple, audio_path, dir_out = None):
+    if dir_out is None:
+        dir_out = os.path.dirname(audio_path)
+
+    chunk_start = chunktuple[0]
+    chunk_end = chunktuple[1]
+    audio_name = os.path.basename(audio_path)
+    chunk_name = re.sub(string=audio_name, pattern="\.mp3$",repl="_s" + chunk_start.__str__() + ".wav")
+    path_out = os.path.join(dir_out, chunk_name)
+
+    subprocess.call([
+        "ffmpeg",
+        "-i", audio_path,  # Input file
+        "-n",  # don't overwrite, just error out
+        "-ar", "16000",  # Audio sample rate
+        "-ac", "1",  # Audio channels
+        "-ss", str(chunk_start),  # Start time
+        "-to", str(chunk_end),  # End time
+        "-c:a", "pcm_s16le",  # Audio codec
+        path_out  # Output path
+    ])
+
+    return path_out
+
+def chunk_raw(audio_path, path_out_base, chunkLength_hr = 1, chunks_to_process = "all"):
+    audio_length = librosa.get_duration(path = audio_path)
+    chunk_length = int(60 * 60 * chunkLength_hr) # in seconds
+
+    chunklist = make_chunklist(audio_length, chunk_length)
+
+    if chunks_to_process == "all":
+        chunks_to_process = list(range(0, len(chunklist)))
+
+    for c in chunks_to_process:
+        take_chunk(chunktuple = chunklist[c], audio_path = audio_path, dir_out= path_out_base)
