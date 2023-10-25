ec6ae9a1e472e89a6ee80f0d85fbf309ee631153
LukeHearon <luke.e.hearon@gmail.com>
2023-10-25 01:05:59 -0400

    lowercase all arguments in buzzdetect.py

--- buzzdetect.py
+++ buzzdetect.py
@@ -6,22 +6,22 @@ parser = argparse.ArgumentParser(
     epilog='github.com/OSU-Bee-Lab/BuzzDetect'
 )
 
-subparsers = parser.add_subparsers(help='sub-command help', dest='action', required = True)
+subparsers = parser.add_subparsers(help='sub-command help', dest='action', required=True)
 
 # analyze
-parser_analyze = subparsers.add_parser('analyze', help = 'analyze something')
-parser_analyze.add_argument('--modelname', required=True, type = str)
-parser_analyze.add_argument('--dir_in', required=False, default = "./audio_in", type = str)
-parser_analyze.add_argument('--dir_out', required=False, default = "./output", type = str)
-parser_analyze.add_argument('--chunkLength', required=False, default = 1, type = float)
-parser_analyze.add_argument('--frameLength', required=False, default = 960, type = int)
-parser_analyze.add_argument('--frameHop', required=False, default = 480, type = int)
+parser_analyze = subparsers.add_parser('analyze', help='analyze something')
+parser_analyze.add_argument('--modelname', required=True, type=str)
+parser_analyze.add_argument('--dir_in', required=False, default="./audio_in", type=str)
+parser_analyze.add_argument('--dir_out', required=False, default="./output", type=str)
+parser_analyze.add_argument('--chunklength', required=False, default=1, type=float)
+parser_analyze.add_argument('--framelength', required=False, default=960, type=int)
+parser_analyze.add_argument('--framehop', required=False, default=480, type=int)
 
 # train
-parser_train = subparsers.add_parser('train', help = 'train a new model')
-parser_train.add_argument('--modelname', required=True, type = str)
-parser_train.add_argument('--epochs', required=False, default = 30, type = int)
-parser_train.add_argument('--trainingset', required=True, type = str)
+parser_train = subparsers.add_parser('train', help='train a new model')
+parser_train.add_argument('--modelname', required=True, type=str)
+parser_train.add_argument('--epochs', required=False, default=30, type=int)
+parser_train.add_argument('--trainingset', required=True, type=str)
 
 # preprocess; work in progress
 # parser_preprocess = subparsers.add_parser('preprocess', help = 'convert audio files into the proper format')
@@ -32,10 +32,12 @@ args = parser.parse_args()
 if (args.action == "train"):
     print("Training new model " + args.modelname + " with set " + args.trainingset)
     from buzzcode.train import *
+
     generate_model(args.modelname, args.trainingset, args.epochs)
 
 elif (args.action == "analyze"):
     from buzzcode.analyze import *
 
     print("analyzing audio in " + args.dir_in + " with model " + args.modelname)
-    analyze_mp3_batch(modelname = args.modelname, directory_in = args.dir_in, directory_out = args.dir_out, frameLength = args.frameLength, frameHop = args.frameHop, chunklength_hr=args.chunkLength)
+    analyze_mp3_batch(modelname=args.modelname, directory_in=args.dir_in, directory_out=args.dir_out,
+                      frameLength=args.framelength, frameHop=args.framehop, chunklength_hr=args.chunklength)
