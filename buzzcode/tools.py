ef1957f49ccdadd9909b8479a4c5c501c4cd882f
LukeHearon <luke.e.hearon@gmail.com>
2023-11-07 13:56:47 -0500

    add function to load flac audio; refactor variable names accordingly

--- buzzcode/tools.py
+++ buzzcode/tools.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow_io as tfio
 import os
 
 def loadUp(modelname):
@@ -12,11 +13,19 @@ def loadUp(modelname):
             classes.append(line.strip())
     return model, classes
 
-def load_wav_16k_mono(filename):
+def load_flac(flac_path):
+    """ Load a FLAC file, convert it to a float tensor """ # I removed resampling as this should be done in preprocessing
+    flac_contents = tf.io.read_file(flac_path)
+    flac = tfio.audio.decode_flac(flac_contents, dtype=tf.int16)
+    return flac
+
+
+
+def load_wav_16k_mono(wav_path):
     """ Load a WAV file, convert it to a float tensor """ # I removed resampling as this should be done in preprocessing
-    file_contents = tf.io.read_file(filename)
+    wav_contents = tf.io.read_file(wav_path)
     wav, sample_rate = tf.audio.decode_wav(
-          file_contents,
+          wav_contents,
           desired_channels=1)
     wav = tf.squeeze(wav, axis=-1)
     return wav
