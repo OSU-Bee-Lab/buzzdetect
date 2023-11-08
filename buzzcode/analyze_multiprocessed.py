b9495b7f11c14d996ff95ffe4d0eea7ad4cbb92a
LukeHearon <luke.e.hearon@gmail.com>
2023-11-08 14:50:36 -0500

    overhaul analyze_multiprocessed.py

--- buzzcode/analyze_multiprocessed.py
+++ buzzcode/analyze_multiprocessed.py
@@ -1,15 +1,19 @@
 import os.path
 import re
-import tensorflow as tf
-import tensorflow_hub as hub
 import pandas as pd
 from buzzcode.tools import unique_dirs, loadUp, load_audio, size_to_runtime
-from buzzcode.process import make_chunklist, take_chunks, take_chunks, cmd_convert
+from buzzcode.chunk import make_chunklist, take_chunks, take_chunks, cmd_chunk
+from buzzcode.convert import cmd_convert
+from buzzcode.analyze import analyze_wav
 from subprocess import Popen
 
+
+
 # change path in to dir in, read files automatically; change chunking dir to processing
-def analyze_multithread(modelname, threads, dir_raw="./audio_in", dir_out="./output", dir_proc="./processing", chunklength=None):
-    model, classes = loadUp(modelname)
+def analyze_multithread(modelname, threads, dir_raw="./audio_in", dir_out=None, dir_proc="./processing", chunklength=None):
+    #
+    # Setup - building control dataframes
+    #   all dataframes will be built before any processing occurs
 
     paths_raw = []
     for root, dirs, files in os.walk(dir_raw):
@@ -17,37 +21,24 @@ def analyze_multithread(modelname, threads, dir_raw="./audio_in", dir_out="./out
             if file.endswith('.mp3'):
                 paths_raw.append(os.path.join(root, file))
 
-    control = pd.DataFrame(paths_raw, columns=["path_raw"])
 
-    # conversion; conversion and chunking will have to happen serially; no easy way around that
+    # control_raw
     #
+    control_raw = pd.DataFrame(paths_raw, columns=["path_raw"])
 
-    dir_converted = os.path.join(dir_proc, "converted")
+    # conversion
+    dir_conv = os.path.join(dir_proc, "converted")
 
     def raw_to_conv(path_raw):
         path_conv = re.sub(pattern=".mp3", repl=".wav",string=path_raw)
-        path_conv = re.sub(pattern=dir_raw, repl=dir_converted,string=path_conv)
+        path_conv = re.sub(pattern=dir_raw, repl=dir_conv,string=path_conv)
         return path_conv
 
-    control['path_conv'] = control['path_raw'].apply(lambda x: raw_to_conv(x))
-    unique_dirs(control['path_conv'])
-
-    control['command_conv'] = list(map(cmd_convert, control['path_raw'], control['path_conv']))
-
-    batches_conv = list(range(0, (len(paths_raw) / threads).__ceil__()))
-    for batch in batches_conv:
-        batch_start = batch * threads
-        batch_end = (batch_start + threads) # index is non-inclusive, so it's fine that this appears 1 too many
-        batch_commands = control['command_conv'][batch_start:batch_end].to_list() # will also drop indices greater than length
-
-        processes = [Popen(cmd, shell=True) for cmd in batch_commands]
-
-        for p in processes:
-            p.wait()
-
+    control_raw['path_conv'] = control_raw['path_raw'].apply(lambda x: raw_to_conv(x))
+    control_raw['cmd_conv'] = list(map(cmd_convert, control_raw['path_raw'], control_raw['path_conv']))
 
     # chunking
-    #
+    dir_chunk = os.path.join(dir_proc, 'chunks')
     chunk_limit = size_to_runtime(3.9)/3600  # greater than 4GB, chunks cause overflow errors; we'll err conservatively
     # is it possible there would be no overflow with riff wavs?
 
@@ -58,22 +49,109 @@ def analyze_multithread(modelname, threads, dir_raw="./audio_in", dir_out="./out
         chunklength = chunk_limit
         print("Desired chunk length causes overflow errors, reducing to " + str(chunk_limit.__round__(1)) + " hours")
 
-    dir_chunk = os.path.join(dir_proc, 'chunks')
 
-    def raw_to_chunkcmd(path_raw):
-        chunk_stub = re.sub(pattern=".mp3", repl="",string=path_raw)
-        chunk_stub = re.sub(pattern=dir_raw, repl=dir_chunk,string=chunk_stub)
+    chunkdf_list = []
+    chunk_stubs = []
+    chunk_cmds = []
 
-        chunklist = make_chunklist(filepath=path_raw, chunklength=chunklength)
-        cmd = make_chunk_command(path_in=path_raw, stub_out=chunk_stub, chunklist=chunklist)
+    for path_raw, path_conv in zip(control_raw['path_raw'], control_raw['path_conv']):
+        chunk_stub = re.sub(pattern=".mp3", repl="",string=path_conv)
+        chunk_stub = re.sub(pattern=dir_conv, repl=dir_chunk,string=chunk_stub)
+        chunk_stubs.append(chunk_stub)
 
-        return cmd
+        chunklist = make_chunklist(filepath=path_raw, chunklength=chunklength) # chunklist from raw because conv doesn't exist yet
+
+        chunk_cmd = cmd_chunk(path_in=path_conv, stub_out=chunk_stub, chunklist=chunklist)
+        chunk_cmds.append(chunk_cmd)
+
+        chunkdf = pd.DataFrame(chunklist, columns=["start", "end"])
+        chunkdf['chunk_stub'] = chunk_stub
+        chunkdf_list.append(chunkdf)
+
+    control_raw['chunk_stub'] = chunk_stubs
+    control_raw['cmd_chunk'] = chunk_cmds
+
+    # control_chunk
+    #
+    control_chunk = pd.concat(chunkdf_list)
+
+    def stub_to_chunk(chunk_stub, start):
+        path_chunk = chunk_stub + "_s" + str(start) + ".wav"
+        return path_chunk
+
+    control_chunk['path_chunk'] = list(map(stub_to_chunk, control_chunk['chunk_stub'], control_chunk['start']))
+
+    # output
+    if dir_out is None:
+        dir_out = os.path.join("./models", modelname, "output")
+
+    def chunk_to_out(path_chunk):
+        path_out = re.sub(pattern=dir_chunk,repl=dir_out,string=path_chunk)
+        path_out = re.sub(pattern=".wav$",repl='_buzzdetect.txt',string=path_out)
+        return(path_out)
+
+    control_chunk['path_out'] = control_chunk['path_chunk'].apply(lambda x: chunk_to_out(x))
+
+    #
+    # directory preparation
+    #
+    dirs_conv = unique_dirs(control_raw['path_conv'], make=True)
+    dirs_chunk = unique_dirs(control_chunk['path_chunk'], make=True)
+    dirs_out = unique_dirs(control_chunk['path_out'], make=True)
+
+    control_raw.to_csv(os.path.join(dir_out, "control_raw.csv"))
+    control_chunk.to_csv(os.path.join(dir_out, "control_chunk.csv"))
 
 
-    control['chunk_stub'], control['chunk_cmd'] = control['path_raw'].apply(lambda x: raw_to_chunkcmd(x))
+    #
+    # defining workers
+    #
+    # I would like these to be verbose, but that would necessitate moving away from the control dataframes
+    # and to...lists? Where the worker handles the processing of filenames, etc.
+
+    def converter(cmd_conv):
+        os.system(cmd_conv)
+
+    def chunker(cmd_chunk):
+        os.system(cmd_chunk)
+
+    model, classes = loadUp(modelname)
+    def analyzer(path_chunk, path_out):
+        results = analyze_wav(model=model, classes=classes, wav_path=path_chunk)
+        results.to_csv(path_out)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def graveyard():
+    batches_conv = list(range(0, (len(paths_raw) / threads).__ceil__()))
+    for batch in batches_conv:
+        batch_start = batch * threads
+        batch_end = (batch_start + threads) # index is non-inclusive, so it's fine that this appears 1 too many
+        batch_commands = control_raw['cmd_conv'][batch_start:batch_end].to_list() # will also drop indices greater than length
+
+        processes = [Popen(cmd, shell=True) for cmd in batch_commands]
+
+        for p in processes:
+            p.wait()
+
+
+    # chunking
+    #
 
     # make chunk dirs
-    unique_dirs(re.sub(dir_raw, dir_chunk, control['path_raw']))
+    unique_dirs(re.sub(dir_raw, dir_chunk, control_raw['path_raw']))
     unique_dirs(control_chunk['path_out'])
 
     batches = list(range(0, (len(control_chunk)/threads).__ceil__()))
