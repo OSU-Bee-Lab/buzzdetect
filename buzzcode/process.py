84731c892ebfe2eb9f0c91518e8487afd829f46d
LukeHearon <luke.e.hearon@gmail.com>
2023-11-08 13:05:28 -0500

    add function to translate path in and out to an ffmpeg conversion command

--- buzzcode/process.py
+++ buzzcode/process.py
@@ -60,7 +60,6 @@ def make_chunk_command_single(path_in, path_out, chunktuple, band_low=200):
 
     return (list2cmdline(cmdlist))
 
-
 def make_chunk_command(path_in, stub_out, chunklist):
     cmdlist = [
         "ffmpeg",
@@ -84,6 +83,23 @@ def make_chunk_command(path_in, stub_out, chunklist):
 
     return list2cmdline(cmdlist)
 
+
+def make_conversion_command(path_in, path_out, band_low=200):
+    cmdlist = [
+        "ffmpeg",
+        "-i", path_in,
+        '-n', # don't overwrite
+        "-v", "quiet", # low verbosity
+        "-stats",
+        "-sample_rate", "16000",  # Audio sample rate
+        "-ac", "1",  # Audio channels
+        "-af", "highpass = f = " + str(band_low),
+        "-c:a", "pcm_s16le"  # Audio codec
+        "-rf64", "always",  # use riff64 to allow large files
+        path_out
+    ]
+
+    return list2cmdline(cmdlist)
 
 def take_chunks(chunklist, path_in_list, path_out_list, band_low=200):
     commands = []
