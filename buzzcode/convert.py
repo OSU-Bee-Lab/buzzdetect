025960d9de81340f2860f9be019426884495b978
LukeHearon <luke.e.hearon@gmail.com>
2023-11-08 14:37:31 -0500

    split process.py into chunk.py and convert.py

---
+++ buzzcode/convert.py
@@ -0,0 +1,18 @@
+from subprocess import list2cmdline
+
+def cmd_convert(path_in, path_out, band_low=200):
+    cmdlist = [
+        "ffmpeg",
+        "-i", path_in,
+        '-n', # don't overwrite
+        "-v", "quiet", # low verbosity
+        "-stats",
+        "-sample_rate", "16000",  # Audio sample rate
+        "-ac", "1",  # Audio channels
+        "-af", "highpass = f = " + str(band_low),
+        "-c:a", "pcm_s16le"  # Audio codec
+        "-rf64", "always",  # use riff64 to allow large files
+        path_out
+    ]
+
+    return list2cmdline(cmdlist)
