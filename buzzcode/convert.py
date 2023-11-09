da4bf5ab6490afcbf78158363155d8ae6c634f13
LukeHearon <luke.e.hearon@gmail.com>
2023-11-09 14:58:30 -0500

    fix fatal tyop in cmd_convert

--- buzzcode/convert.py
+++ buzzcode/convert.py
@@ -10,7 +10,7 @@ def cmd_convert(path_in, path_out, band_low=200):
         "-sample_rate", "16000",  # Audio sample rate
         "-ac", "1",  # Audio channels
         "-af", "highpass = f = " + str(band_low),
-        "-c:a", "pcm_s16le"  # Audio codec
+        "-c:a", "pcm_s16le",  # Audio codec
         "-rf64", "always",  # use riff64 to allow large files
         path_out
     ]
