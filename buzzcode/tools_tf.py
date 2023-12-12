83b355defc46368de514acf6825ff9b7f140aff6
LukeHearon <luke.e.hearon@gmail.com>
2023-12-12 11:22:10 -0500

    update loadup for new storage of classes in weight preserve backwards compatibility for now

--- buzzcode/tools_tf.py
+++ buzzcode/tools_tf.py
@@ -1,19 +1,32 @@
 import tensorflow as tf
 import tensorflow_io as tfio
 import tensorflow_hub as hub
+import pandas as pd
 import os
 
 
 def loadup(modelname):
-    model = tf.keras.models.load_model(os.path.join("./models/", modelname))
+    dir_model = os.path.join("./models/", modelname)
+    model = tf.keras.models.load_model(dir_model)
 
-    classes = []
-    with open(os.path.join("models/", modelname, "classes.txt"), "r") as file:
-        # Use a for loop to read each line from the file and append it to the list
-        for line in file:
-            # Remove the newline character and append the item to the list
-            classes.append(line.strip())
-    return model, classes
+    classpath_txt =os.path.join(dir_model, "classes.txt") # backwards compatibility for previous models; can remove later
+    classpath_csv = os.path.join(dir_model, "weights.csv")
+
+    if os.path.exists(classpath_csv): # prefer weights if available
+        df = pd.read_csv(classpath_csv)
+        classes = df['classification']
+        return model, list(classes)
+
+    elif os.path.exists(classpath_txt):
+        classes = []
+        with open(os.path.join("models/", modelname, "classes.txt"), "r") as file:
+            # Use a for loop to read each line from the file and append it to the list
+            for line in file:
+                # Remove the newline character and append the item to the list
+                classes.append(line.strip())
+        return model, classes
+    else:
+        raise OSError("no file containing classes found in model directory")
 
 def get_yamnet():
     os.environ["TFHUB_CACHE_DIR"]="./yamnet"
