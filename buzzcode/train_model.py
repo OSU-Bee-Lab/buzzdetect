6ed785236bc14c3c3d5dd3798d8b50f0cb42b243
LukeHearon <luke.e.hearon@gmail.com>
2024-01-25 15:30:57 -0500

    reduce get_metadata() for new metadata approach new metadata will allow multiple metadata files for differential training of models on the same audio files

--- buzzcode/train_model.py
+++ buzzcode/train_model.py
@@ -5,11 +5,8 @@ import sys
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-import soundfile as sf
 import os
-import re
 import librosa
-from buzzcode.utils.tools import search_dir
 from buzzcode.utils.analysis import get_yamnet, load_audio_tf
 from buzzcode.test_model import analyze_testFold
 
@@ -20,38 +17,20 @@ tf.config.threading.set_intra_op_parallelism_threads(1)
 #
 yamnet = get_yamnet()
 
-# dir_training="./training"
-def get_metadata(dir_training, invalidate = False):
-    paths_audio = search_dir(dir_training, list(sf.available_formats().keys()))
-    cachepath = os.path.join(dir_training, "metadata.csv")
 
-    metadata = pd.DataFrame()
-    metadata['path'] = paths_audio
-
-    if (not os.path.exists(cachepath)) or invalidate:
+def get_metadata(path_metadata):
+    metadata = pd.read_csv(path_metadata)
+    if 'duration' not in metadata.columns:
         metadata['duration'] = [librosa.get_duration(path=file) for file in metadata['path']]
-        metadata['classification'] = [re.search("_s\\d+_(.*)\\.wav", p).group(1) for p in metadata['path']]
 
-        metadata.to_csv(cachepath)
-        return metadata
+    return metadata
 
-    df_cache = pd.read_csv(cachepath)
 
-    # drop cached values
-    metadata = metadata[-metadata['path'].isin(df_cache['path'])]
-    metadata['duration'] = [librosa.get_duration(path=file) for file in metadata['path']]
-    metadata['classification'] = [re.search("_s\\d+_(.*)\\.wav", p).group(1) for p in metadata['path']]
-
-    df_out = pd.concat([metadata, df_cache[df_cache['path'].isin(paths_audio)]])
-
-    return df_out
-
-# modelname="test"; dir_training="./training"; drop_threshold=0; path_weights=None; test_model=False; cpus=None; memory_allot=None
-def generate_model(modelname, dir_training="./training", drop_threshold=0, path_weights=None, test_model=False, cpus=None, memory_allot=None):
+def generate_model(modelname, path_metadata="./training/metadata/metadata.csv", drop_threshold=0, path_weights=None, test_model=False, cpus=None, memory_allot=None):
     if test_model and (cpus is None or memory_allot is None):
         sys.exit("cpu count and memory allotment must be given when testing model")
 
-    dir_model = os.path.join("models/", modelname)
+    dir_model = os.path.join("./models/", modelname)
 
     if os.path.exists(dir_model):
         raise FileExistsError('a model folder with this name already exists; delete or rename the existing model folder and re-run')
@@ -60,7 +39,7 @@ def generate_model(modelname, dir_training="./training", drop_threshold=0, path_
 
     # Acquiring and filtering training data
     #
-    metadata = get_metadata(dir_training)
+    metadata = get_metadata(path_metadata)
 
     if drop_threshold > 0:
         classes_current = metadata.classification.unique()
@@ -86,6 +65,7 @@ def generate_model(modelname, dir_training="./training", drop_threshold=0, path_
         weightdf = pd.read_csv(path_weights)
         weights_new = pd.DataFrame()
 
+        # if a weight isn't assigned to a class, assign it 1
         weights_new['classification'] = [c for c in classes if c not in weightdf['classification'].values]
         weights_new['weight'] = 1
 
