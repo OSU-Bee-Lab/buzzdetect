6011c4311423d7b8eaf171f8338db611795907bf
luke <luke.e.hearon@gmail.com>
2025-10-09 16:44:07 -0400

    add conf.py

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# -- Project information -----------------------------------------------------
+
+project = 'buzzdetect'
+copyright = '2025, OSU Bee Lab'
+author = 'OSU Bee Lab'
+
+# The short X.Y version
+version = '1.0'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.1'
+
+
+# -- General configuration ---------------------------------------------------
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'alabaster'
