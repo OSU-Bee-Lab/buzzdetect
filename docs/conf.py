0c775338790bb12ec4d30a1b5a068ec05fcab71a
luke <luke.e.hearon@gmail.com>
2025-10-09 21:14:01 -0400

    update docs files for sphinx

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# -- Project information -----------------------------------------------------
+
+project = 'buzzdetect'
+copyright = '2025, OSU Bee Lab'
+author = 'OSU Bee Lab'
+
+# The short X.Y version
+version = '1.0'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.1'
+
+
+# -- General configuration ---------------------------------------------------
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'sphinx_rtd_theme'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # for Google/Numpy style docstrings
+    'sphinx_rtd_theme',
+]
