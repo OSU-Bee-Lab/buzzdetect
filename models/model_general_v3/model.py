7beffb490e241daaf056becefb75a66ebd62028e
luke <luke.e.hearon@gmail.com>
2025-10-23 16:15:16 -0400

    critical: add missing files from object-oriented refactor in f35e0f80

---
+++ models/model_general_v3/model.py
@@ -0,0 +1,31 @@
+import os
+
+import buzzcode.config as cfg
+from buzzcode.models.BaseModel import BaseModel
+
+class ModelGeneralV3(BaseModel):
+    modelname = "model_general_v3"
+    embeddername = 'yamnet_k2'
+    digits_results = 2
+
+    def initialize(self):
+        self.embedder.initialize()
+
+        from keras.layers import TFSMLayer
+        dir_model = os.path.abspath(os.path.join(cfg.DIR_MODELS, self.modelname))
+        self.model = TFSMLayer(dir_model, call_endpoint='serving_default')  # self.model defined in ABC
+
+    def predict(self, audiosamples):
+        """
+        Generate predictions for audio data
+
+        Args:
+            audiosamples: numpy array of audio samples at self.embedder.samplerate
+
+        Returns:
+            tensor results
+        """
+        embeddings = self.embedder.embed(audiosamples)
+        results = self.model(embeddings)['dense']
+
+        return results
